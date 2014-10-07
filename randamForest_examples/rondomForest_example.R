3eb143c8bd48972b14c0e322a7fcd2664c0c4cf5
egawa <tatsuya@TatsuyaEgawa-no-MacBook-Air.local>
2014-10-07 22:18:57 +0900

    add randamForest_example.R

---
+++ randamForest_examples/rondomForest_example.R
@@ -0,0 +1,19 @@
+# install.package("randomForest")
+library(randomForest)
+library(rpart) 
+
+# random sampling
+n <- nrow(iris)
+s <- sample(n,n*0.5)
+iris.train <- iris[s,]
+iris.test <- iris[-s,]
+
+# random forest
+forest <- randomForest(Species~., data=iris.train, ntree=500)
+pred.forest <- predict(forest, newdata=iris.test, type="class")
+table(pred.forest, iris.test[,5])
+
+# decision tree
+tree <- rpart(Species~.,data=iris.train)
+pred.rpart<-predict(tree, iris.test, type="class")
+table(pred.rpart, iris.test[,5])
