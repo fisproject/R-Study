f2ce94b6e00eec88776590f591d70e2045bf8b30
egawa <Tatsuya@TEgawa-no-MacBook-Air.local>
2014-12-24 09:06:02 +0900

    add knn-examples

---
+++ knn_examples/knn-example.R
@@ -0,0 +1,22 @@
+library(class)
+
+# wine <- data(wine)
+wine <- read.csv("R-Study/knn_examples/wine.data",header=FALSE)
+colnames(wine) <- c("class","Alcohol","Malic Acid","Ash","Alcalinity of Ash","Magnesium",
+                    "Total Phenols","Flavanoids","Nonflavanoid Phenols","Proanthocyanins",
+                    "Color Intensity","Hue","0D280/OD315 of Diluted Wines","Proline")
+
+# train
+train <- rbind(wine[1:30,2:14], wine[60:90,2:14], wine[131:161,2:14]) 
+train.label <- factor(c(wine[1:30,1], wine[60:90,1], wine[131:161,1]))
+
+# test
+test <- rbind(wine[31:59,2:14], wine[61:130,2:14], wine[161:178,2:14]) 
+test.label <- factor(c(wine[31:59,1], wine[61:130,1], wine[161:178,1]) )
+
+# knn
+res <- knn(train,test,train.label,k=3,prob=TRUE)
+attributes(res)
+
+# accuracy
+sum(res==test.label)/length(test.label)
