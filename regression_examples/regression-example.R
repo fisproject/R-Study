30461238f74751f7a21af488d6a1d57bb2c67f7f
egawa <tatsuya@TatsuyaawanoAir.lan>
2015-01-31 23:05:41 +0900

    add regression_examples

---
+++ regression_examples/regression-example.R
@@ -0,0 +1,22 @@
+# install.packages("rgl")
+library(rgl)
+
+air <- na.omit(airquality)
+pairs(airquality[,1:4],pch=21,bg="red",cex=1)
+
+air.lm <-lm(air$Ozone~air$Solar.R+air$Wind+air$Temp, data=air)
+summary(air.lm)
+
+step(air.lm)
+
+plot3d(air$Wind, air$Temp, air$Ozone,
+	xlab = "wind", ylab = "temp", zlab = "ozone",
+  size = 1, type = "s", col = "blue")
+
+# add predict
+air.pre = predict(air.lm)
+plot3d(air$Wind, air$Temp, air.pre,
+	xlab = "wind", ylab = "temp", zlab = "ozone",
+  size = 1, type = "s", add = TRUE, col = "red")
+
+writeWebGL(width=500, height=550)
---
+++ regression_examples/regression-surface3d-example.R
@@ -0,0 +1,30 @@
+# install.packages("rgl")
+library(rgl)
+
+air <- na.omit(airquality)
+x1 <- air$Wind
+x2 <- air$Temp
+
+air.lm <-lm(air$Ozone~(x1+x2)^2, data=air)
+summary(air.lm)
+
+sapply(air, range)
+#       Ozone Solar.R Wind Temp Month Day
+# [1,]     1       7  2.3   57     5   1
+# [2,]   168     334 20.7   97     9  31
+
+x1 <- seq(from = 2.3, to = 20.7, by = 0.1)
+x2 <- seq(from = 57, to = 97, by = 1)
+
+air.sur <- outer(X = x1, Y = x2, FUN = function(x1, x2) {
+    predict(air.lm, newdata = data.frame(x1 = x1, x2 = x2))
+})
+
+plot3d(x1, x2, air$Ozone,
+		xlab = "wind", ylab = "temp", zlab = "ozone",
+		size = 1, type = "s", col = "blue")
+
+surface3d(x = x1, y = x2, z = air.sur,
+          col = "red", alpha = 0.5)
+
+writeWebGL(width=500, height=550)
