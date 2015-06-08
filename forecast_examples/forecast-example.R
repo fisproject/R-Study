de34fdf9adcd3191d7f02b05c912907b06bba83f
egawa <Tatsuya@TEgawa-no-MacBook-Air.local>
2015-06-08 09:03:18 +0900

    Add forecast_examples/ quantmod_examples/

---
+++ forecast_examples/forecast-example.R
@@ -0,0 +1,65 @@
+# install.packages("forecast")
+require(forecast)
+
+# change working directory
+frame_files <- lapply(sys.frames(), function(x) x$ofile)
+frame_files <- Filter(Negate(is.null), frame_files)
+setwd(dirname(frame_files[[length(frame_files)]]))
+
+# convert char-code "nkf -w --overwrite filename"
+d <- read.csv("data/7201.T.csv", header=T)
+
+# 5 years
+raw <- 245*5
+# convert header "Date","Open","High","Low","Close","Volume"
+data <- data.frame(
+          Date=d$日付[1:raw],
+          Open=d$始値[1:raw],
+          High=d$高値[1:raw],
+          Low=d$安値[1:raw],
+          Close=d$終値[1:raw],
+          Volume=d$出来高[1:raw],
+          Adjusted=d$調整後終値[1:raw]
+        )
+
+data.zoo <- zoo(data$Close, order.by=as.Date(data$Date, format='%Y/%m/%d'))
+# time series
+data.ts <- ts(data.zoo)
+
+# Plots a time series along with its acf (AutoCorrelation Function) and either its pacf (Partial ACF),
+# lagged scatterplot or spectrum.
+par(family="HiraKakuProN-W3")
+tsdisplay(data.ts, main="日産")
+
+model <- auto.arima(data.ts,
+            max.p=2, # AR degree
+            max.q=2, # MA degree
+            ic="aic",# model selection.
+            trace=T, # reported option
+            stepwise=T
+         )
+# ARIMA(2,1,2) with drift         : 10281.96
+# ARIMA(0,1,0) with drift         : 10279.18
+# ARIMA(1,1,0) with drift         : 10279.32
+# ARIMA(0,1,1) with drift         : 10280.52
+# ARIMA(0,1,0)                    : 10278.63
+# ARIMA(1,1,1) with drift         : 10281.26
+#
+# Best model: ARIMA(0,1,0) with drift
+
+summary(model)
+# sigma^2 estimated as 260.4:  log likelihood=-5136.17
+# AIC=10276.34   AICc=10276.35   BIC=10286.56
+#
+# Training set error measures:
+#                        ME     RMSE      MAE         MPE     MAPE     MASE
+# Training set 0.0005219955 16.12392 12.06049 -0.02388146 1.412625 1.000159
+#                    ACF1
+# Training set 0.02431441
+# Training set 0.09790725
+
+f <- forecast(model,
+        level=c(50, 95), # confidence interval
+        h=20*3 # after 3 months
+     )
+plot(f)
---
+++ quantmod_examples/quantmod-example.R
@@ -0,0 +1,38 @@
+# https://github.com/joshuaulrich/quantmod
+# install.packages("quantmod")
+require(quantmod)
+
+# change working directory
+frame_files <- lapply(sys.frames(), function(x) x$ofile)
+frame_files <- Filter(Negate(is.null), frame_files)
+setwd(dirname(frame_files[[length(frame_files)]]))
+
+# convert char-code "nkf -w --overwrite filename"
+d <- read.csv("data/7201.T.csv", header=T)
+
+# 2 years
+raw <- 245*2
+# convert header "Date","Open","High","Low","Close","Volume"
+data <- data.frame(
+          Date=d$日付[1:raw],
+          Open=d$始値[1:raw],
+          High=d$高値[1:raw],
+          Low=d$安値[1:raw],
+          Close=d$終値[1:raw],
+          Volume=d$出来高[1:raw],
+          Adjusted=d$調整後終値[1:raw]
+        )
+
+# head(data)
+#       Date   Open   High    Low  Close   Volume  Adjusted
+# 1  2015/6/3 1322.0 1350.0 1319.5 1319.5 16815000   1319.5
+# 2  2015/6/2 1313.0 1326.5 1300.5 1326.0 14139600   1326.0
+# 3  2015/6/1 1293.0 1304.0 1282.5 1302.5  8270000   1302.5
+# 4 2015/5/29 1300.0 1308.5 1289.5 1300.5 10061300   1300.5
+# 5 2015/5/28 1298.5 1312.0 1293.0 1299.0 14069200   1299.0
+# 6 2015/5/27 1270.0 1291.0 1262.0 1280.5 12494400   1280.5
+
+ohlc <- read.zoo(data, header=T)
+
+par(family="HiraKakuProN-W3")
+candleChart(ohlc, name="日産", multi.col=TRUE, theme="white")
