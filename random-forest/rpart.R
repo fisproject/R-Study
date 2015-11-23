8c2de541b4c85226dccf93d627cfb5d34e73c280
ttsy <tatsuya@TatsuyaEgawa-no-MacBook-Air.local>
2015-11-23 20:43:43 +0900

    Fix random-forest/randomforest.R

---
+++ random-forest/rpart.R
@@ -0,0 +1,28 @@
+require(rpart)
+require(RColorBrewer)
+
+set.seed(1500)
+
+# random sampling
+n <- nrow(iris)
+s <- sample(n, n * 0.5)
+iris.train <- iris[s,]
+iris.test <- iris[-s,]
+
+# decision tree
+tree <- rpart(
+  Species ~ .,
+  data=iris.train
+)
+
+pred.tree <- predict(
+  tree,
+  iris.test,
+  type='class'
+)
+
+table(pred.tree, iris.test[,5])
+#   pred.tree   setosa versicolor virginica
+#   setosa         30          0         0
+#   versicolor      0         14         1
+#   virginica       0          3        27
