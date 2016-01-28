af2cfd3799a01e5e95e9817caafefc83cc1cf9f6
ttsy <tatsuya@TatsuyaEgawa-no-MacBook-Air.local>
2016-01-28 23:40:32 +0900

    Add ggplot2/subplot-example.R

--- ggplot2/ggplot2-example.R
+++ ggplot2/ggplot2-example.R
@@ -11,28 +11,23 @@ p <- ggplot(
   d,
   aes(
       x = Sepal.Length,
-      y = Sepal.Width
+      y = Sepal.Width,
+      colour=Species
   )
 )
 
-# Gradient
-g <- p + geom_point(aes(size=Petal.Length, colour=Petal.Width), alpha=1) +
-      labs(title="Iris", x="Length", y="Width") +
-      scale_colour_gradient(low="cyan", high="magenta")
-plot(g)
-ggsave("img/iris-gradient.png", g)
-
 # Hue
-g <- p + geom_point(aes(size=Petal.Length, colour=Species), alpha=1) +
-      labs(title="Iris", x="Length", y="Width") +
-      scale_colour_hue()
+g <- p + geom_point() +
+    labs(title="Iris", x="Sepal.Length", y="Sepal.Width") +
+    scale_colour_hue()
+    # scale_colour_gradient(low="cyan", high="magenta")
 plot(g)
 ggsave("img/iris-hue.png", g)
 
 # Smoothing
-g <- p + geom_point(colour="magenta") +
-      labs(title="Iris", x="Length", y="Width") +
-      stat_smooth(method="lm")
+g <- p + geom_point() +
+    labs(title="Iris", x="Sepal.Length", y="Sepal.Width") +
+    stat_smooth(method="lm")
 plot(g)
 ggsave("img/iris-smooth.png", g)
 
@@ -48,7 +43,7 @@ p <- ggplot(
 )
 
 g <- p + geom_histogram(alpha=0.3, position="identity", size=1) +
-      labs(title="Iris", x="Length") +
+      labs(title="Iris", x="Sepal.Length") +
       geom_density(alpha=0)
 plot(g)
 ggsave("img/iris-hist.png", g)
@@ -67,7 +62,7 @@ plot(g)
 ggsave("img/iris-tile.png", g)
 
 # Tile
-g <- qplot(round(Sepal.Length), round(Sepal.Width), fill=Petal.Length, data=d, geom="tile") +
-      scale_fill_gradient(limits=c(0, 6), low="white", high="black")
+g <- qplot(Sepal.Length, Sepal.Width, fill=Petal.Length, data=d, geom="tile") +
+    scale_fill_gradient(limits=c(0, 6), low="white", high="black")
 plot(g)
 ggsave("img/iris-tile-qplot.png", g)
---
+++ ggplot2/subplot-example.R
@@ -0,0 +1,18 @@
+require(ggplot2)
+require(ggfortify)
+library(purrr)
+
+d <- iris
+
+p1 <- ggplot(d, aes(Sepal.Width, Sepal.Length, color=Species)) + labs(title="Sepal")
+p2 <- ggplot(d, aes(Petal.Width, Petal.Length, color=Species)) + labs(title="Petal")
+p3 <- ggplot(d, aes(Sepal.Length, Petal.Length, color=Species)) + labs(title="Length")
+p4 <- ggplot(d, aes(Sepal.Width, Petal.Width, color=Species)) + labs(title="Width")
+
+p <- new('ggmultiplot', plots=list(p1, p2, p3, p4), ncol=2)
+p[1:4] <- p[1:4] + geom_point() + stat_smooth(method = 'lm')
+p
+
+# cluster number 1 to 9
+kclusts <- purrr::map(1:9, ~ kmeans(d[-5], .))
+autoplot(kclusts, data=d[-5], ncol=3) + theme(legend.position="none")
