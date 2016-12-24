81351bb2e6a9ec9b9f6dddcd7e0217eeacabf935
ttsy <japan.t.egawa@gmail.com>
2016-12-24 16:19:03 +0900

    Add discrete-choice-model/ordered-logit-clm.R

---
+++ discrete-choice-model/ordered-logit-clm.R
@@ -0,0 +1,15 @@
+require(ordinal)
+
+head(MASS::housing)
+
+class(MASS::housing$Sat)
+
+# ordered logit mode
+house.logit <- clm(Sat ~ Infl + Type + Cont, weights = Freq, data = MASS::housing)
+summary(house.logit, digits = 3)
+
+confint(house.logit)
+
+predict(house.logit, MASS::housing, type = "p")
+
+texreg::screenreg(house.logit)
