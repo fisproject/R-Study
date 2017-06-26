940bfdaa80c7c5b8ad1c05603392e4541960dc72
t2sy <japan.t.egawa@gmail.com>
2017-06-26 22:00:25 +0900

    Add data-generation/rcauchy.R

---
+++ data-generation/rcauchy.R
@@ -0,0 +1,23 @@
+require(MASS)
+require(ggplot2)
+
+frame_files <- lapply(sys.frames(), function(x) x$ofile)
+frame_files <- Filter(Negate(is.null), frame_files)
+setwd(dirname(frame_files[[length(frame_files)]]))
+
+set.seed(12345)
+
+n <- 1000
+df <- data.frame(
+  X1 = rcauchy(n, location = 0, scale = 1),
+  X2 = rcauchy(n, location = 0, scale = 1)
+)
+
+# save as csv
+fname <- "./data/2d-cauchy.csv"
+write.csv(df, fname, row.names = FALSE)
+
+# plot
+p <- ggplot() + geom_point(data = df, aes(x = X1, y = X2)) +
+  labs(title = fname, x = "X1", y = "X2")
+plot(p)
