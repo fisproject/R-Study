efeca9e7a6ea3bb26e884eec92069958d47f2872
t2sy <japan.t.egawa@gmail.com>
2018-05-19 10:47:26 +0900

    Add data-preprocessing-and-visualization/lubridate/lubridate.R

---
+++ data-preprocessing-and-visualization/lubridate/lubridate.R
@@ -0,0 +1,30 @@
+library(dplyr)
+library(tibble)
+
+df <- tibble(dt = c("2018-01-01T01:00:00",
+                    "2018-04-10T12:30:20",
+                    "2018-08-01T14:50:30",
+                    "2018-10-10T14:50:30"))
+
+df_ct <- df %>%
+  mutate(dt_ct = as.POSIXct(dt, format = "%Y-%m-%dT%H:%M:%S")) %>%
+  mutate(
+    year = year(dt_ct),
+    month = month(dt_ct),
+    days = days_in_month(dt_ct),
+    wday = wday(dt_ct),
+    weekdays = weekdays(dt_ct),
+    hour = hour(dt_ct),
+    minute = minute(dt_ct),
+    second = second(dt_ct),
+    dt_str = format(dt_ct, "%Y-%m-%d %H:%M:%S"))
+
+df_season <- df_ct %>%
+  mutate(
+    season = factor(
+      case_when(
+        month >= 3 & month < 6 ~ "spring",
+        month >= 6 & month < 9 ~ "summer",
+        month >= 9 & month < 12 ~ "autumn",
+        TRUE ~ "winter"),
+      levels = c("spring", "summer", "autumn", "winter")))
